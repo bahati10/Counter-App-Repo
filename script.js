7747959edeb04ca08805d85325bdd1dbf1571175
Bahati <bahatiyves10@gmail.com>
2022-11-07 08:14:19 -0500

    disabled button when its  =  0
    counter app is fumctional now

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let count = 0;
 document.getElementById("decreaseBtn").onclick = function() {
     if(count > 0){
         count -=1;
+    }else {
+        alert("The count can't under zero");
     }
 
     document.getElementById("counter").innerHTML = count;
-
 }
 
 
