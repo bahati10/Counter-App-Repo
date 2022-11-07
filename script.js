8ae3b211993c2d5c8af705c03e57e108310323c5
Bahati <bahatiyves10@gmail.com>
2022-11-07 04:46:25 -0500

    added buttons functionality
    script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function() {
+    if(count > 0){
+        count -=1;
+    }
+
+    document.getElementById("counter").innerHTML = count;
+
+}
 
 
+document.getElementById("resetBtn").onclick = function() {
+    count=0;
+    document.getElementById("counter").innerHTML = count;
+}
+
+
+document.getElementById("increaseBtn").onclick = function() {
+    count+=1;
+    document.getElementById("counter").innerHTML = count;
+}
